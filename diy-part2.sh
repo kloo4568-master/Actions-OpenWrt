26efd5ce02d2145989741c55d7c6b5f27d1e4fee
kloo4568-master <kloo4568@gmail.com>
2026-07-10 22:36:42 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 默认IP修改
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 默认主机名
+sed -i "s/OpenWrt/ZBT-WR8305RT/g" package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 默认WiFi名称
+sed -i "s/OpenWrt/ZBT-OpenWrt/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 删除不需要的语言包，节省空间
+rm -rf feeds/luci/modules/luci-base/po/zh_Hans
+
+# 编译优化
+echo "MT7620A firmware build"
